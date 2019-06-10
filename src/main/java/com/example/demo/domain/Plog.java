c4f9af13051f9d551b38c16ac9fd4773fe26ab26
Ankhaa <Ankhaa@192.168.0.199>
2019-06-10 13:08:41 +0800

    Changing with some error message Hoping it will work.

--- src/main/java/com/example/demo/domain/Plog.java
+++ src/main/java/com/example/demo/domain/Plog.java
@@ -1,5 +1,6 @@
-package com.example.demo.Domain;
+package com.example.demo.domain;
 
 public class Plog {
+	
 
 }
--- src/main/java/com/example/demo/repository/BlogRepository.java
+++ src/main/java/com/example/demo/repository/BlogRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
-class BlogRepository {
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BlogRepository extends PagingAndSortingRepository<Blog, Integer>{
 
 }
