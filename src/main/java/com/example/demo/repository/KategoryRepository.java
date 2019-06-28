422055035d54676c3fdf30e826f09d9b006df26c
Ankhaa <buyandelgerjan@gmail.com>
2019-06-28 09:46:07 +0800

    Delete working

--- src/main/java/com/example/demo/repository/KategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.example.demo.domain.kategor;
-
-public interface KategoryRepository extends PagingAndSortingRepository<kategor, Integer>{
-
-}
